4ad8f1b43e9327eb08d9c5da4bab31f51a3ce4c3
kiinegi <kiinegi1227@gmail.com>
2021-03-07 00:42:55 +0900

    各種機能を追加

---
+++ twitter-clone/src/compornents/Tweetinput.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import styles from "./Tweetinput.module.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth, storage, db } from "../firebase";
+import { Avatar, Button, IconButton } from "@material-ui/core";
+import firebase from "firebase/app";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+
+const Tweetinput : React.FC = () => {
+  const user = useSelector(selectUser);
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const [tweetMsg, setTweetMsg] = useState("");
+
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (tweetImage) {
+      const S =
+        "abcdefgfijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + tweetImage.name;
+      const uploadTweetImg = storage.ref(`images/${fileName}`).put(tweetImage);
+      uploadTweetImg.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        () => {},
+        (err) => {
+          alert(err.message);
+        },
+        async () => {
+          await storage
+            .ref("images")
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMsg,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              });
+            });
+        }
+      );
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMsg,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setTweetImage(null);
+    setTweetMsg("");
+  };
+
+  return (
+    <>
+      <form onSubmit={sendTweet}>
+        <div className={styles.tweet_form}>
+          <Avatar
+            className={styles.tweet_avatar}
+            src={user.photoUrl}
+            onClick={async () => {
+              await auth.signOut();
+            }}
+          />
+          <input
+            className={styles.tweet_input}
+            placeholder="What's happening?"
+            type="text"
+            autoFocus
+            value={tweetMsg}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setTweetMsg(e.target.value)
+            }
+          />
+          <IconButton>
+            <label>
+              <AddAPhotoIcon
+                className={
+                  tweetImage ? styles.tweet_addIconLoaded : styles.tweet_addIcon
+                }
+              />
+              <input
+                className={styles.tweet_hiddenIcon}
+                type="file"
+                onChange={onChangeImageHandler}
+              />
+            </label>
+          </IconButton>
+        </div>
+        <button
+          type="submit"
+          disabled={!tweetMsg}
+          className={
+            tweetMsg ? styles.tweet_sendBtn : styles.tweet_sendDisableBtn
+          }
+        >
+          Tweet
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Tweetinput;
--- twitter-clone/src/features/userSlice.ts
+++ twitter-clone/src/features/userSlice.ts
@@ -9,14 +9,14 @@ interface USER {
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    user: {uid:"", photoUrl:"", displayName:""}
+    user: { uid: "", photoUrl: "", displayName: "" }
   },
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
     },
     logout: (state) => {
-      state.user = {uid:"",photoUrl:"",displayName:""};
+      state.user = { uid: "", photoUrl: "", displayName: "" };
     },
     updateUserProfile: (state, action: PayloadAction<USER>) => {
       state.user.displayName = action.payload.displayName;
